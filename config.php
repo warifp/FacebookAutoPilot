404d0ac55517304fb151c3b77e93cc535d1459bd
Wahyu Arif Purnomo <warifp@gmail.com>
2019-01-20 00:04:53 +0700

    Update config.php
    Update appID and appSecret

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php 
 	session_start();
 
-	$appId = ''; 
-	$appSecret = '';
+	$appId = '165210707507361'; 
+	$appSecret = 'e0b6dc2d747efe1e7d6546e66337f2d3';
 	$requiredPermissions = 'public_profile, publish_actions, user_groups'; 
-	$redirectURL = ''; // FB will redirect to this page with a code
+	$redirectURL = 'http://localhost'; // FB will redirect to this page with a code
  	
  	$minDelayTime = 20; // Set the min delay in seconds between api requests
 	$maxDelayTime = 40; // Set the max delay in seconds between api requests
